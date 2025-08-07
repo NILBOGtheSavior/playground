c1d26e81cb7fd738b4cd29555a009a6633f2a355
NILBOGtheSavior <naztechmedia@gmail.com>
2025-08-07 12:48:20 -0500

    updates

--- c/1_apprentice/detab.c
+++ c/1_apprentice/detab.c
@@ -6,6 +6,36 @@
  * a variable or a symbolic parameter?
  */
 
-#import <stdio.h>
+#include <stdio.h>
 
+#define MAXLINE 1000
 
+void detab(int n);
+
+int main() {
+	// detab(4);
+	detab(12);
+	return(0);
+}
+
+void detab(int n) {
+	char ct[MAXLINE];
+	int c;
+	int i = 0;
+
+	while ((c=getchar()) != EOF && i < MAXLINE - 1) {
+		if (c == '\t') {
+			// add spaces
+			int s = n - (i % n);
+
+			for (int j = 0; j < s; j++) {
+				ct[i++] = ' ';
+			}
+		} else {
+			// add to ct
+			ct[i++] = c;
+		}
+	}
+	ct[i] = '\0';
+	printf("%s\n", ct);
+}
---
+++ c/1_apprentice/entab.c
@@ -0,0 +1,27 @@
+// Exercise 1-21
+/* Write a program 'entab' that replaces strings 
+ * of blanks by the minimum number if tabs and 
+ * blanks to achieve the same spacing. Use the 
+ * same tab stops as for detab. When either a tab
+ * or a single blank would suffice to reach a tab
+ * stop, which should be given preference?
+ */
+#include <stdio.h>
+
+#define MAXLINES 1000
+
+void entab(int n);
+
+int main() {
+	return 0;
+}
+
+void entab(int n) {
+	char input[MAXLINES];
+	int c;
+	int i = 0;
+
+	while ((c = getchar()) != EOF && i < MAXLINES - 1) {
+
+	}
+}
