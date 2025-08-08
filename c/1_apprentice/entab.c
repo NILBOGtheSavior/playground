506d491bc04e7a3b7a09c01bbb1e976c09030b64
NILBOGtheSavior <naztechmedia@gmail.com>
2025-08-08 16:57:28 -0500

    updates

--- c/1_apprentice/entab.c
+++ c/1_apprentice/entab.c
@@ -1,27 +1,54 @@
 // Exercise 1-21
 /* Write a program 'entab' that replaces strings 
- * of blanks by the minimum number if tabs and 
- * blanks to achieve the same spacing. Use the 
+ * of blanks by the minimum number of tabs and   
+ * blanks to achieve the same spacing. Use the   
  * same tab stops as for detab. When either a tab
  * or a single blank would suffice to reach a tab
  * stop, which should be given preference?
  */
+
 #include <stdio.h>
+#include <stdbool.h>
 
-#define MAXLINES 1000
+#define MAXLINE 1000
 
-void entab(int n);
+void entab();
+
+bool inspace = false;
 
 int main() {
+	entab();
 	return 0;
 }
 
-void entab(int n) {
-	char input[MAXLINES];
+void entab() {
+	bool inspace;
 	int c;
+	int o[MAXLINE];
 	int i = 0;
+	int b, e;
 
-	while ((c = getchar()) != EOF && i < MAXLINES - 1) {
-
+	while ((c = getchar()) != EOF && i < MAXLINE - 1) {
+		if (c == ' ') {
+			if (!inspace) {
+				b = i;
+				inspace = true;
+			}
+		} else (
+			if (inspace) {
+				e = i;
+				inspace = false;
+				respace(b, e);
+				b = e = 0;
+			}
+			o[i] = c;
+		)
+		i++;
 	}
+
 }
+
+void respace(b, e) {
+	
+}
+
